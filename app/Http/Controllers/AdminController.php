ce69b50e853256b1778e2541285b52cdb5aaeb70
pazu <1550520500@qq.com>
2016-08-25 17:37:34 +0800

    sort by near

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -15,7 +15,7 @@ class AdminController extends Controller
     {
         $name = Session::get('name');
         if ($name = 'wuyang') {
-            $ret = DB::select("select username,sum(acctinputoctets)/1024/1024 'in' ,sum(acctoutputoctets)/1024/1024 'out',sum(acctinputoctets)/1024/1024+sum(acctoutputoctets)/1024/1024 total,sum(acctsessiontime) alltime,max(acctstoptime) near from radacct group by username order by total desc limit 100");
+            $ret = DB::select("select username,sum(acctinputoctets)/1024/1024 'in' ,sum(acctoutputoctets)/1024/1024 'out',sum(acctinputoctets)/1024/1024+sum(acctoutputoctets)/1024/1024 total,sum(acctsessiontime) alltime,max(acctstoptime) near from radacct group by username order by near desc limit 100");
             $data = array('data' => $ret);
             return view('admin', $data);
         } else {
